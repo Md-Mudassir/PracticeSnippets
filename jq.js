60e430a3a34e261320f23608066caf817b0e5b6a
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-05 10:43:15 +0530

    changes

---
+++ jq.js
@@ -0,0 +1,7 @@
+let weather = $(".weather-app");
+let cityWeather = $("#weather-app .city-name");
+console.log(cityWeather.html());
+cityWeather.html("wow");
+console.log(cityWeather.html());
+let cityWeather1 = $("#weather-app .city-weather");
+console.log(cityWeather1.html());
