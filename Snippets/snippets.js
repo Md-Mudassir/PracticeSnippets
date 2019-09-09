9cf671e29b5ae232577985fce1e3ba85ec612710
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-09 21:14:51 +0530

    practice

--- Snippets/snippets.js
+++ Snippets/snippets.js
@@ -1,27 +1,87 @@
-// // Create square of numbers.
-// let aray = [144, 9, 16];
-// // let sq = 0;
-// // for (let i = 0; i < aray.length; i++) {
-// //   sq = aray[i] ** 2;
-// //   console.log(sq);
-// // }
-// // let sqq = aray.forEach(x => (n = x ** 2));
-// const greet = () => "hello,world";
-// console.log(greet());
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
 
-// let people = ["m", "r", "d", "f"];
+// Set token
+let _token = hash.access_token;
 
-// const arr = (letter, index) => {
-//   console.log(`${index} and ${letter}`);
-// };
+const authEndpoint = "https://accounts.spotify.com/authorize";
 
-// people.forEach(arr);
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "2102d6bf57714410a8f50dd1ccadc571";
+const redirectUri = "https://spotify-web-playback.glitch.me";
+const scopes = [
+  "streaming",
+  "user-read-birthdate",
+  "user-read-private",
+  "user-modify-playback-state"
+];
 
-const ul = document.querySelector(".city");
-const people = ["m", "r", "d", "f"];
-let html = "";
-people.forEach(function(person) {
-  html += `<li>${person}</li>`;
-});
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
+}
 
-console.log(html);
+// Set up the Web Playback SDK
+
+window.onSpotifyPlayerAPIReady = () => {
+  const player = new Spotify.Player({
+    name: "Web Playback SDK Template",
+    getOAuthToken: cb => {
+      cb(_token);
+    }
+  });
+
+  // Error handling
+  player.on("initialization_error", e => console.error(e));
+  player.on("authentication_error", e => console.error(e));
+  player.on("account_error", e => console.error(e));
+  player.on("playback_error", e => console.error(e));
+
+  // Playback status updates
+  player.on("player_state_changed", state => {
+    console.log(state);
+    $("#current-track").attr(
+      "src",
+      state.track_window.current_track.album.images[0].url
+    );
+    $("#current-track-name").text(state.track_window.current_track.name);
+  });
+
+  // Ready
+  player.on("ready", data => {
+    console.log("Ready with Device ID", data.device_id);
+
+    // Play a track using our new device ID
+    play(data.device_id);
+  });
+
+  // Connect to the player!
+  player.connect();
+};
+
+// Play a specified track on the Web Playback SDK's device ID
+function play(device_id) {
+  $.ajax({
+    url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
+    type: "PUT",
+    data: '{"uris": ["spotify:track:5ya2gsaIhTkAuWYEMB0nw5"]}',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer " + _token);
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
