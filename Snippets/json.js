1d2dd43ebd6ce23b2b8365ff964ec1111962bb7e
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-11 16:11:21 +0530

    json

--- Snippets/json.js
+++ Snippets/json.js
@@ -1,51 +1,39 @@
-// // our array
-// let alpha = ["a", "b", "c", "d", "e", "f"];
+// our array
+let alpha = ["a", "b", "c", "d", "e", "f"];
 
-// // storing our array as a string
-// localStorage.setItem("letters", JSON.stringify(alpha));
+// storing our array as a string
+localStorage.setItem("letters", JSON.stringify(alpha));
 
-// let retrievedData = localStorage.getItem("letters");
-// let alpha2 = JSON.parse(retrievedData);
+let retrievedData = localStorage.getItem("letters");
+let alpha2 = JSON.parse(retrievedData);
 
-// console.log(retrievedData);
-// console.log(alpha2);
+console.log(retrievedData);
+console.log(alpha2);
 
-// let testObject = { one: 1, two: 2, three: 3 };
 
-// let keys = Object.keys(testObject);
-// console.log(keys);
+$(function() {
+  getJSON();
+});
 
-// localStorage.setItem("testObject", JSON.stringify(testObject));
 
-// let retrievedObject = localStorage.getItem("testObject");
-
-// console.log("retrievedObject: ", JSON.parse(retrievedObject));
-
-// console.log(retrievedObject);
-
-// $(function() {
-//   // data in attributes
-//   // getAttributeData()
-//   getJSON();
-//   // getLocalData()
-// });
-// function getJSON() {
-//   $.getJSON("data.json", function(json) {
-//     questions = json;
-//     // console.log(Object)
-//     const keys = Object.keys(questions);
-//     console.log(keys.length);
-//     // [0 -> 1] * 1
-//     let randIndex = Math.round(Math.random() * (keys.length - 1));
-//     // console.log(randIndex);
-//   });
-// }
-var allQuestions = new Array();
-
-function loadQuestions() {
-  $.getJSON("data.json", function(data) {
-    console.log(data.quiz);
-  }).error(function() {
-    console.log("error: json not loaded");
+function getJSON() {
+  $.getJSON("data.json", function(json) {
+    questions = json;
+    // console.log(Object);
+    const keys = Object.keys(questions);
+    //storing the objects as string in local  storage
+    localStorage.setItem("ques", JSON.stringify(questions));
+    let retrievedObject = localStorage.getItem("ques");
+    //getting the values in form of objects
+    console.log("retrievedObject: ", JSON.parse(retrievedObject));
+    // console.log(localStorage.getItem(questions));
+    // console.log(questions);
+    let randIndex = Math.floor(Math.random() * (keys.length));
+    //parsing into objects 
+    let que = JSON.parse(localStorage.getItem("ques"));
+    //getting a random index for question
+    let varible = que[keys[randIndex]];
+    console.log(varible["question"]);
+    console.log(varible["answer"]);
   });
 }
