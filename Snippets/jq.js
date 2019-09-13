46b1f8cf69e154f3a423e55d913db8be9f9758ea
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-13 19:23:30 +0530

    practice

--- Snippets/jq.js
+++ Snippets/jq.js
@@ -1,7 +1,6 @@
-let weather = $(".weather-app");
-let cityWeather = $("#weather-app .city-name");
-console.log(cityWeather.html());
-cityWeather.html("wow");
-console.log(cityWeather.html());
-let cityWeather1 = $("#weather-app .city-weather");
-console.log(cityWeather1.html());
+function testJS() {
+  var name = jQuery("#name").val();
+  jQuery.load("next.html", function() {
+    jQuery("#here").html(name);
+  });
+}
