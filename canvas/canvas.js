1c7bc8e7ce47f736cf43f3a8337775f5fbbf67f7
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-17 17:13:45 +0530

    arc

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,17 +1,28 @@
 let canvas = document.querySelector("canvas");
 
-canvas.width = innerWidth - 40;
-canvas.height = innerHeight - 20;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
+//rec
 let c = canvas.getContext("2d");
 c.fillStyle = "red";
 c.fillRect(120, 120, 100, 100);
 c.fillStyle = "green";
 c.fillRect(190, 90, 200, 200);
 
+//line
 c.beginPath();
 c.moveTo(30, 100);
 c.lineTo(200, 10);
 c.lineTo(600, 80);
 c.lineTo(400, 80);
 c.stroke();
+
+//arc
+for (let i = 0; i <= 4; i++) {
+  let x = Math.random() * window.innerHeight;
+  let y = Math.random() * window.innerWidth;
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.stroke();
+}
