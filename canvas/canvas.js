d9623834b647ab65cc62ccfcae26097f33a7e5b0
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-17 18:05:06 +0530

    bounce

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -31,15 +31,41 @@ let c = canvas.getContext("2d");
 //   c.strokeStyle = "blue";
 //   c.stroke();
 // }
-let x = 200;
 
+function Circle(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+  };
+}
+
+let circle = new Circle(200, 200);
+
+let x = 200;
+let dx = 4;
+let radius = 30;
+let y = 100;
+let dy = 4;
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerHeight, innerWidth);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  circle.draw();
   c.beginPath();
-  c.arc(x, 300, 30, 0, Math.PI * 2, false);
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
   c.strokeStyle = "blue";
   c.stroke();
-  x++;
+  if (x + radius > innerWidth || x - radius < 0) {
+    dx = -dx;
+  }
+  if (y + radius > innerHeight || y - radius < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
 }
 animate();
