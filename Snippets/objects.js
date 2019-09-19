de5211b071bb2c2bebffe7aefe40aa7edf8d84d8
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-19 23:44:51 +0530

    classes

---
+++ Snippets/objects.js
@@ -0,0 +1,44 @@
+const book = {
+  name: "YOLO",
+  year: "2010",
+  author: "md",
+  getSummary: function() {
+    return `its name is ${this.name} and published in ${this.year} and writen by ${this.author}`;
+  }
+};
+
+console.log(book.name);
+
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+// const booker = new Book("hello", "md", 2242);
+
+Book.prototype.getDetails = function(titler) {
+  return `${(this.title = titler)} & ${this.title}`;
+};
+
+function magzine(title, author, year, month) {
+  Book.call(this, title, author, year);
+  this.month = month;
+}
+
+const mag1 = new magzine("yamla", "md", 333, 23);
+
+// console.log(mag1);
+class CBook {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+  getSum() {
+    return `${this.title}`;
+  }
+}
+
+const books = new CBook("you", "md", 8987);
+console.log(books.getSum());
