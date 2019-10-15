f37f9703c68ad82a687e56632dcd0db783d7b21f
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-15 18:50:45 +0530

    example

--- DS & Algo/myArray.js
+++ DS & Algo/myArray.js
@@ -22,8 +22,20 @@ class MyArray {
         this.shiftItems(index);
         return item;
       }
+
       shiftItems(index) {
         for (let i = index; i < this.length - 1; i++) {
           this.data[i] = this.data[i + 1];
         }
+    }
 }
+const myArray = new MyArray();
+myArray.push('hi');
+myArray.push('you');
+myArray.push('!');
+myArray.pop();
+myArray.deleteAtIndex(0);
+myArray.push('are');
+myArray.push('nice');
+myArray.shiftItems(0);
+console.log(myArray);
