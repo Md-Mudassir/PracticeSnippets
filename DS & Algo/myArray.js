7058931e642707bff703a7807d3f3520c1aa18ba
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-15 18:49:00 +0530

    added pop method

--- DS & Algo/myArray.js
+++ DS & Algo/myArray.js
@@ -11,4 +11,11 @@ class MyArray {
         this.length++;
         return this.data;
       }
+      pop() {
+        // const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        // return lastItem;
+        
+      }
 }
