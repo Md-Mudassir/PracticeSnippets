28adf7d5f8efe49e1181bd78819f568d8277e790
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-16 16:59:26 +0530

    update hash

--- DS & Algo/hashes.js
+++ DS & Algo/hashes.js
@@ -8,23 +8,61 @@
 
 // console.log(recurring([1, 2, 2, 3, 1, 2]));
 
-// class HashTable {
-//     constructor(size){
-//       this.data = new Array(size);
-//     }
-//     get(x){
-//       return this.data[x]
-//     }
-//     _hash(key) {
-//       let hash = 0;
-//       for (let i =0; i < key.length; i++){
-//           hash = (hash + key.charCodeAt(i) * i) % this.data.length
-//       }
-//       return hash;
-//     }
-//   }
-
-//   const myHashTable = new HashTable(50);
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+      // this.data = [];
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+  
+    set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+  
+    get(key){
+      const address = this._hash(key);
+      const currentBucket = this.data[address]
+      if (currentBucket) {
+        for(let i = 0; i < currentBucket.length; i++){
+          if(currentBucket[i][0] === key) {
+            return currentBucket[i][1]
+          }
+        }
+      }
+      return undefined;
+    }
+    
+    keys(){
+      const keysArray = [];
+      console.log(this.data.length);
+      for (let i = 0; i < this.data.length; i++){
+        if(this.data[i]){
+          keysArray.push(this.data[i][0][0])
+        }
+      }
+      return keysArray;
+    }
+  }
+  
+  const myHashTable = new HashTable(50);
+  myHashTable.set('grapes', 10000)
+  myHashTable.set('grapes', 10000)
+  myHashTable.get('grapes')
+  myHashTable.set('apples', 9)
+  myHashTable.get('apples')
+  myHashTable.keys()
 
 let mapy = new Map([[1, 2], [2, 3]]);
 console.log(mapy);
@@ -33,3 +71,4 @@ console.log(mapy);
 
 // console.log("Map1");
 // console.log(map1);
+
