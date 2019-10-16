a1339d63ee4c23d6f9fa9c7f655cf40ee904405d
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-16 16:27:44 +0530

    lilnkedlist

--- DS & Algo/hashes.js
+++ DS & Algo/hashes.js
@@ -1,27 +1,35 @@
-function recurring(inp) {
-  for (let i = 0; i < inp.length; i++) {
-    for (let j = i + 1; j < inp.length; j++) {
-      if (inp[i] === inp[j]) return inp[i];
-    }
-  }
-}
+// function recurring(inp) {
+//   for (let i = 0; i < inp.length; i++) {
+//     for (let j = i + 1; j < inp.length; j++) {
+//       if (inp[i] === inp[j]) return inp[i];
+//     }
+//   }
+// }
 
-console.log(recurring([1, 2, 2, 3, 1, 2]));
+// console.log(recurring([1, 2, 2, 3, 1, 2]));
 
-class HashTable {
-    constructor(size){
-      this.data = new Array(size);
-    }
-    get(x){
-      return this.data[x]
-    }
-    _hash(key) {
-      let hash = 0;
-      for (let i =0; i < key.length; i++){
-          hash = (hash + key.charCodeAt(i) * i) % this.data.length
-      }
-      return hash;
-    }
-  }
-  
-  const myHashTable = new HashTable(50);
+// class HashTable {
+//     constructor(size){
+//       this.data = new Array(size);
+//     }
+//     get(x){
+//       return this.data[x]
+//     }
+//     _hash(key) {
+//       let hash = 0;
+//       for (let i =0; i < key.length; i++){
+//           hash = (hash + key.charCodeAt(i) * i) % this.data.length
+//       }
+//       return hash;
+//     }
+//   }
+
+//   const myHashTable = new HashTable(50);
+
+let mapy = new Map([[1, 2], [2, 3]]);
+console.log(mapy);
+
+// let map1 = new Map([[1, 2], [2, 3], [4, 5]]);
+
+// console.log("Map1");
+// console.log(map1);
