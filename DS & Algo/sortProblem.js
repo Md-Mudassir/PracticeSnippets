23eba2364134843375d32f4bf86164e12ab7e5c0
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-17 15:32:32 +0530

    bubblesort

--- DS & Algo/sortProblem.js
+++ DS & Algo/sortProblem.js
@@ -1,10 +1,29 @@
 //Initial Solution
 
 let list = [1, 2, 3, 4],
-list2 = [4, 2, 7, 0];
+  list2 = [4, 2, 7, 0];
 let list3 = [...list, ...list2];
 let sorted = list3.sort((a, b) => a - b);
 
-console.log(sorted);
+// console.log(sorted);
 
 //From Scratch Solution
+
+//Bubble sort
+const array = [1, 2, 6, 4, 8, 5, 6, 8, 9, 0];
+
+function sortEle(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+sortEle(array);
+
+console.log(array);
