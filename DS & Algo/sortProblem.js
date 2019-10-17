a88a86b623e8b4231c6f318cc000276410a97365
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-17 15:05:30 +0530

    sorted

--- DS & Algo/sortProblem.js
+++ DS & Algo/sortProblem.js
@@ -1,10 +1,8 @@
-let list = [1,2,3,4];
-let list2=[4,2,7,0]
-
-const sorted = list.sort(function(a, b) {
+let list = [1, 2, 3, 4];
+let list2 = [4, 2, 7, 0];
+let list3 = [...list, ...list2];
+const sorted = list3.sort(function(a, b) {
   return a - b;
 });
-
-
 
 console.log(sorted);
