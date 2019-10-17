8a62d0d0e9db4b6752ed8a0e97dc625040e9b9d4
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-17 17:53:09 +0530

    recursion

--- DS & Algo/recursion.js
+++ DS & Algo/recursion.js
@@ -2,7 +2,14 @@ function factorial(number) {
   if (number == 2) {
     return number;
   }
+  debugger;
   return number * factorial(number - 1);
 }
 
-console.log(factorial(5));
+function fiboncci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fiboncci(n - 1) + fiboncci(n - 2);
+}
+console.log(fiboncci(21));
