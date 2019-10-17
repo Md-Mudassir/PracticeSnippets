1c4144cb78b080f54897dd36647ee5ea831d1da5
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-17 23:06:04 +0530

    insertion sort

--- DS & Algo/sortProblem.js
+++ DS & Algo/sortProblem.js
@@ -19,3 +19,20 @@ sortEle = arr => {
 };
 sortEle(array);
 console.log("using bubble sort " + array);
+
+//Insertion sort
+const insertionSort = arr => {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let el = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > el; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = el;
+  }
+  return arr;
+};
+
+console.log(insertionSort(array))
