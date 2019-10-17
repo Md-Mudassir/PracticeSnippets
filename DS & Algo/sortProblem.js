dfc2f65814f8de9f4fd37f69dba57d425ed7fd41
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-17 15:07:31 +0530

    reduced sort

--- DS & Algo/sortProblem.js
+++ DS & Algo/sortProblem.js
@@ -1,8 +1,6 @@
 let list = [1, 2, 3, 4];
 let list2 = [4, 2, 7, 0];
 let list3 = [...list, ...list2];
-const sorted = list3.sort(function(a, b) {
-  return a - b;
-});
+const sorted = list3.sort((a, b) => a - b);
 
 console.log(sorted);
