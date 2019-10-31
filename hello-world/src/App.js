6b2aaf87f97cf8289fc7b28403364862839e0d85
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-31 17:21:07 +0530

    json data

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -19,11 +19,20 @@ class App extends Component {
       ]
     };
   }
+
+  async componentDidMount() {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => console.log(users));
+
+  }
   render() {
     return (
       <div className="App">
         {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
+          <h1 key={monster.id}>
+            {monster.name},{monster.id}
+          </h1>
         ))}
       </div>
     );
