ee7084cbe39b001bc11ac0a10e59edebec4c325a
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-18 13:04:52 +0530

    jest tesing, gatsby 101

--- Snippets/snippets.js
+++ Snippets/snippets.js
@@ -1,7 +1,27 @@
-let name = "md mudassir";
-console.log(`${name}`.repeat(8));
-console.log(name.startsWith("m"));
+// let name = "md mudassir";
+// console.log(`${name}`.repeat(8));
+// console.log(name.startsWith("m"));
 
-let [hi, hello] = [1, 2];
+// let [hi, hello] = [1, 2];
 
-console.log(hi);
+// console.log(hi);
+
+// function multi(x) {
+//   return function(y) {
+//     return y * x;
+//   };
+// }
+// let ans = multi(4);
+// console.log(ans(2));
+
+let car_year = 2010;
+const car_price =
+  car_year == 2015
+    ? 0.1
+    : car_year == 2013
+    ? 0.12
+    : car_year == 2010
+    ? 0.13
+    : null;
+
+console.log(car_price);
---
+++ Testing/sum.test.js
@@ -0,0 +1,5 @@
+const sum=require('./sum')
+
+test('adds two numbers',()=>{
+    
+})
