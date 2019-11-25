357d319b7550e8f2e241b485b889c981dcd1ccd8
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-25 15:17:43 +0530

    dicts and tuples

--- python snippets/dictionaries.py
+++ python snippets/dictionaries.py
@@ -1,1 +1,29 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+# Create dict
+# like objects in js
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
+# Get dict keys
+print(person.keys())
+
+# Get dict items
+print(person.items())
+
+# Copy dict
+person2 = person.copy()
+person2['city'] = 'Boston'
+
+del(person2['age'])
+print(person2)
+
+# lists of dicts
+
+people = [
+    {'name': 'md', 'age': 22},
+    {'name': 'muddu', 'age': 2}
+]
+print(people[0])
+print(people[1]['name'])
--- python snippets/tuples_sets.py
+++ python snippets/tuples_sets.py
@@ -1,5 +1,17 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# create
+
+fruits = ('apple', 'orange', 'watermelon')
+print(fruits)
+
+fruits1 = fruits
+print(type(fruits1))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruits_set = {'apple', 'orange'}
+fruits_set.add('apple')
+fruits_set.add('Grape')
+
+# fruits_set.clear()
+print(fruits_set)
