8edc3a50a59b463810cd3bf1ded64b5bb929f8e3
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-25 16:10:29 +0530

    modules

--- python snippets/modules.py
+++ python snippets/modules.py
@@ -1,1 +1,22 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# from camelcase import CamelCase
+import datetime
+from datetime import date
+import time
+from validator import validate_email
+
+timestamp = time.time()
+
+today = date.today()
+
+print(today, timestamp)
+
+# c = CamelCase()
+# print(c.hump('hello mr world'))
+
+email = 'test&tes.com'
+if(validate_email(email)):
+    print('valid')
+else:
+    print('invalid')
