dc5116dc6a71b612a14bad03f82991fec371d790
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-25 15:52:22 +0530

    loops & conditionals

--- python snippets/loops.py
+++ python snippets/loops.py
@@ -1,5 +1,20 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['md', 'shaik', 'kiran']
 
+for person in people:
+    print(f'current name is {person}')
+
+# range
+for i in range(len(people)):
+    print(people[i])
+
+for i in range(0, 11):
+    print(f'Number: {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+
+count = 0
+while count < 10:
+    print(f'Count: {count}')
+    count += 1
