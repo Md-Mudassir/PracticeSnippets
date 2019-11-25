725931d7869e97959de3b86e90d8adfc72cac216
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-25 16:30:14 +0530

    json and files

--- python snippets/classes.py
+++ python snippets/classes.py
@@ -1,2 +1,33 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def ageis(self):
+        return self.age
+
+
+md = User('mudassir', 22)
+print(md.age)
+
+# Extend class
+
+
+class Customer(User):
+    # Constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+
+abrar = Customer('abrar', 25)
+print(abrar.ageis())
+
+# print(bal)
--- python snippets/py_json.py
+++ python snippets/py_json.py
@@ -1,1 +1,9 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+# sample
+userJSON = '{"first_name": "John", "last_name": "Doe", "age": 30}'
+
+user = json.loads(userJSON)
+
+print(user)
+print(user['last_name'])
