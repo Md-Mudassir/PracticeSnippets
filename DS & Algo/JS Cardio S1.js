81a14d156d6c6837e5c5868b598761d293557e3e
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-28 15:07:53 +0530

    fizzbuzz

--- DS & Algo/JS Cardio S1.js
+++ DS & Algo/JS Cardio S1.js
@@ -11,7 +11,7 @@ reverseString = str => {
   //   let revStr = "";
   //   str.split("").forEach(char => (revStr = char + revStr));
   ///////////////////////////
-  //   return str.split("").reduce((revStr, char) => char + revStr);
+  return str.split("").reduce((revStr, char) => char + revStr);
 };
 
 // CHALLENGE 2: VALIDATE A PALINDROME
@@ -26,12 +26,25 @@ isPalindrome = str => {
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
+reverseInt = int => {
+  const revString = int
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(revString) * Math.sign(int);
+};
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.substr(1))
+    .join(" ");
+}
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
@@ -40,9 +53,21 @@ function maxCharacter(str) {}
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
 
 // Call Function
-const output = isPalindrome("racecar");
+const output = fizzBuzz();
 
 console.log(output);
